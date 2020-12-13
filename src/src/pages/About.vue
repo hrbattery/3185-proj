4c5a648b6e762b18d0fa87dafab2dfb186d4f2d0
HRBattery <hrbattery@gmail.com>
2020-12-13 15:27:37 +0800

    fixes

--- src/src/pages/About.vue
+++ src/src/pages/About.vue
@@ -9,7 +9,7 @@
         <p></p>
         <p>This is a web page for the project of CSC3185, in order to introduce computer vision.</p>
         <p>The web page is built with Vuetify and Nginx. All the designs of pages are supported by Vuetify, and the application is deployed on the server by Webpack and Nginx.</p>
-        <p>The repository of the project is private. <a href="https://github.com/hrbattery/3185-proj"></a></p>
+        <p>The repository of the project is private. <a href="https://github.com/hrbattery/3185-proj">https://github.com/hrbattery/3185-proj</a></p>
         <p>Thanks for reading.</p>
       </div>
       </v-container>
